746250a3ecdd8f12bce1ab6d5f947396eb6f2e34
4cyk4 <124710673+4cyk4@users.noreply.github.com>
2024-05-15 08:59:16 +0000

    ajout n1

---
+++ regle_mails.kt
@@ -0,0 +1,15 @@
+//Query qui a partir d'une liste de sender typé "spam" ressort la liste de sender ayant envoyé des mails avec le memes sujet
+// Liste des senders d'origine
+let liste_senders = dynamic(["info@infodespros.fr"]);
+// Q1 Liste des sujets a partir des senders
+let sujets = EmailEvents
+    where SenderFromAddress in (liste_senders)
+    project Subject
+    distinct Subject;
+// Q2 jonction de la table des sujets extraits avec tous les mails pour extraire les senders
+EmailEvents
+    join kind=inner (
+    sujets
+) on Subject
+    summarize by SenderFromAddress, Subject
+//  summarize by SenderFromAddress
